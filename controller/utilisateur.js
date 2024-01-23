a66d25ccacdd4c3a1cdf814c0bb25e5a40c3c47f
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2024-01-23 13:23:04 +0100

    Implementation controller CRUD utilisateur

--- controller/utilisateur.js
+++ controller/utilisateur.js
@@ -1,22 +1,88 @@
-const { Utilisateur } = require('../model/utilisateur');
-const client = require('../db/connect');
-
-
-
+const { Utilisateur } = require("../model/utilisateur");
+const { ObjectID } = require("mongodb");
+const client = require("../db/connect");
 
 const ajouterUtilisateur = async (req, res) => {
-    try{
-        let utilisateur = new Utilisateur(req.body.noms, req.body.adresse, req.body.telephone);
-       let result = await  client.db().collection('utilisateurs').insertOne(utilisateur);
+  try {
+    let utilisateur = new Utilisateur(
+      req.body.noms,
+      req.body.adresse,
+      req.body.telephone
+    );
+    let result = await client
+      .db()
+      .collection("utilisateurs")
+      .insertOne(utilisateur);
 
-       res.status(200).json(result);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(501).json(error);
+  }
+};
 
-    } catch(error){
-        console.log(error);
-        res.status(500).json(error);
-
+const getUtilisateur = async (req, res) => {
+  try {
+    let cursor = client
+      .db()
+      .collection("utilisateurs")
+      .find()
+      .sort({ noms: 1 });
+    let result = await cursor.toArray();
+    if (result.length > 0) {
+      res.status(200).json(result);
+    } else {
+      res.status(204).json({ msg: "Aucun utilisateur trouvé" });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(501).json(error);
+  }
+};
 
+const updateUtilisateur = async (req, res) => {
+  try {
+    let noms = req.body.noms;
+    let adresse = req.body.adresse;
+    let telephone = req.body.telephone;
+    let result = await client
+      .db()
+      .collection("utilisateurs")
+      .updateOne({ _id: id }, { $set: { noms, adresse, telephone } });
 
-module.exports = {ajouterUtilisateur};
+    if (result.modifiedCount === 1) {
+      res.status(200).json({ msg: "Modification réussie" });
+    } else {
+      res.status(404).json({ msg: "Cet utilisateur n'existe pas" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(501).json(error);
+  }
+};
+
+const deleteUtilisateur = async (req, res) => {
+  try {
+    let id = new ObjectID(req.params.id);
+    let result = await client
+      .db()
+      .collection("utilisateurs")
+      .deleteOne({ _id: id });
+    if (result.deletedCount === 1) {
+      res.status(200).json({ msg: "Suppression réussie" });
+    } else {
+      res.status(404).json({ msg: "Cet utilisateur n'existe pas" });
+    }
+  } catch (error) {
+    console.log(error);
+
+    res.status(501).json(error);
+  }
+};
+
+module.exports = {
+  ajouterUtilisateur,
+  getUtilisateur,
+  updateUtilisateur,
+  deleteUtilisateur,
+};
