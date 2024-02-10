9bd6daaff978be486c0ef64bbe4af18a64fcf7b4
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2024-02-10 03:36:49 +0100

    Requete axios pour afficher utilisateurs

---
+++ client/src/components/userList.vue
@@ -0,0 +1,49 @@
+<template>
+  <table class="table is-striped is-bordered">
+    <thead>
+      <tr>
+        <th>Nom</th>
+        <th>Adresse</th>
+        <th>Téléphone</th>
+        <th>Pays</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="user_alias in User" :key="user_alias.id">
+        <td>{{ user_alias.noms }}</td>
+        <td>{{ user_alias.adresse }}</td>
+        <td>{{ user_alias.telephone }}</td>
+        <td>{{ user_alias.country }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import axios from 'axios'
+/* eslint-disable */
+export default {
+  name: 'User',
+  data() {
+    return {
+      User: []
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:3000/api/v1/utilisateurs/')
+      .then((response) => {
+        console.log(response.data)
+        this.User = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style>
+/* Aucun style spécifique n'est nécessaire, vous pouvez supprimer cette section si vous n'en avez pas besoin */
+</style>
---
+++ client/src/views/UserListview.vue
@@ -0,0 +1,9 @@
+<script setup>
+import userList from '@/components/userList.vue'
+</script>
+
+<template>
+  <main>
+    <userList />
+  </main>
+</template>
